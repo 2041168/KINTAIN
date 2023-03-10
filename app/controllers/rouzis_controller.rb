e35eceba37d0f4f7f7aa1ea88167d56e19204f66
2041168 <g2041168@tcu.ac.jp>
2023-03-10 22:23:07 +0000

    DB対応

--- app/controllers/rouzis_controller.rb
+++ app/controllers/rouzis_controller.rb
@@ -3,13 +3,13 @@ class RouzisController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @yoteis = @user.yoteis.all
+    @yoteis = @yoteis.order(:yoteikaishi)
     @rouzis = @user.rouzis.where.not(zitsukaishi: nil).where.not(zitsuowari: nil)
+    @rouzis = @rouzis.order(:rouzikaishi)
     
     # 絞り込み範囲の指定
     @kensakukaishi = params.dig(:kensakukaishi)&.to_date || Time.zone.today
     @kensakuowari = params.dig(:kensakuowari)&.to_date || 30.days.from_now.to_date
-    @kk = @kensakukaishi.to_datetime.beginning_of_day
-    @ko = @kensakuowari.to_datetime.end_of_day
     
     if @kensakuowari < @kensakukaishi
         flash.now[:alert] = "終了日は開始日より後にしてください"
@@ -20,20 +20,18 @@ class RouzisController < ApplicationController
         render :index
     end
     
-    # 範囲内の予定と労働時間の取得
-    yoteis = @yoteis.select { |y| y.yoteikaishi >= @kk && y.yoteikaishi <= @ko }
-    rouzis = @rouzis.select { |r| r.zitsukaishi >= @kk && r.zitsukaishi <= @ko }
-    @yoteide = yoteis
-    
+    # 予定と労働時間の取得
+    yoteis = @user.yoteis.where(yoteikaishi: @kensakukaishi.to_datetime.beginning_of_day..@kensakuowari.to_datetime.end_of_day)
+    rouzis = @user.rouzis.where(zitsukaishi: @kensakukaishi.to_datetime.beginning_of_day..@kensakuowari.to_datetime.end_of_day)
     
     # 日付ごとに予定と労働時間をグループ化
     @data = {}
     (@kensakukaishi..@kensakuowari).each do |date|
-      # dateと日付が同じ予定を取得
+      # 予定の取得
       yotei_data = yoteis.select { |y| y.yoteikaishi.to_date == date }
       yotei = yotei_data.map { |y| { id: y.id, time: "#{y.yoteikaishi.strftime('%-H:%M')}~#{y.yoteiowari.strftime('%-H:%M')}" } }
     
-      # dateと日付が同じ労働を取得
+      # 労働時間の取得
       rouzi_data = rouzis.select { |r| r.zitsukaishi.to_date == date }
       rouzi = rouzi_data.map { |r| { id: r.id, time: "#{r.zitsukaishi.strftime('%-H:%M')}~#{r.zitsuowari.strftime('%-H:%M')}" } }
     
@@ -91,42 +89,30 @@ class RouzisController < ApplicationController
       end
     else
       flash.now[:alert] = "全ての情報を入力してください"
-      render :show
+        render :show
     end
   end
   
   def new
     @user = User.find(params[:user_id])
-    teigi
     @rouzi = @user.rouzis.new
+    teigi
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, alert: 'ユーザーが見つかりませんでした'
   end
 
   def create
     @user = User.find(params[:user_id])
-    if params[:rouzi][:zitsukaishi].present? && params[:rouzi][:zitsukaishi_time].present? && params[:rouzi][:zitsuowari].present? && params[:rouzi][:zitsuowari_time].present?
-      #zkt = Time.zone.parse(params.dig(:zitsukaishi_time))
-      #zot = Time.zone.parse(params.dig(:zitsuowari_time))
-      zitsukaishi = DateTime.parse(params[:rouzi][:zitsukaishi])
-      zkdate_str = zitsukaishi.strftime('%Y-%m-%d')
-      zitsukaishi_t = DateTime.parse(params[:rouzi][:zitsukaishi_time])
-      zktime_str = zitsukaishi_t.strftime('%H:%M')
-      zkdatetime_str = "#{ zkdate_str } #{ zktime_str }"
-      zkdatetime = DateTime.parse(zkdatetime_str)
-      
-      zitsuowari = DateTime.parse(params[:rouzi][:zitsukaishi])
-      zodate_str = zitsuowari.strftime('%Y-%m-%d')
-      zitsuowari_t = DateTime.parse(params[:rouzi][:zitsuowari_time])
-      zotime_str = zitsuowari_t.strftime('%H:%M')
-      zodatetime_str = "#{ zodate_str } #{ zotime_str }"
-      zodatetime = DateTime.parse(zodatetime_str)
+    if params[:rouzi][:zitsukaishi] != '' && params[:rouzi][:zitsukaishi_time] != '' && params[:rouzi][:zitsuowari] != '' && params[:rouzi][:zitsuowari_time] != ''
+      zkdatetime_str = "#{params[:rouzi][:zitsukaishi].to_date} #{params[:rouzi][:zitsukaishi_time]}"
+      zkdatetime = Time.zone.parse(zkdatetime_str)
+      zodatetime_str = "#{params[:rouzi][:zitsuowari].to_date} #{params[:rouzi][:zitsuowari_time]}"
+      zodatetime = Time.zone.parse(zodatetime_str)
       @rouzi = @user.rouzis.new(zitsukaishi: zkdatetime, zitsuowari: zodatetime, user_id: @user.id)
-
-      if zodatetime < zkdatetime
+      if @rouzi.zitsuowari < @rouzi.zitsukaishi
         flash.now[:alert] = "終了時刻は開始時刻より後にしてください"
         render :new
-      elsif  @rouzi.save
+      elsif @rouzi.save
         redirect_to user_rouzi_path(@user, @rouzi), notice: "作成しました"
       end
     else
